8d4c826240cb8ad1612980268fa61237d9f88d0e
Osvaldo Ramirez <osvaldo97@live.com.ar>
2022-04-13 21:19:33 -0300

    agregando info

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import s from './ItemDetail.module.css'
 
-export default function ItemDetail({image, name, price, id}){
+export default function ItemDetail({image, name, price, id, description, stock}){
     return (
         <div className={s.containerDetail} key={id}>
           <img src={image} alt={name} className = {s.image} />
           <div>
             <h2>{name}</h2>
             <h3>${price}</h3>
+            <p>{description}</p>
+            <p>{stock}</p>
           </div>
         </div>
         
